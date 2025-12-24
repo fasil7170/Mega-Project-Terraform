95f62f593a36e4d62bc83b20751099e9daed837e
fasil7170 <fasilmubarak07@gmail.com>
2025-12-25 02:21:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "aws_eks_node_group" "devopsshack" {
     min_size     = 1
   }
 
-  instance_types = ["t2.medium"]
+  instance_types = ["t3.medium"]
 
   depends_on = [aws_eks_cluster.devopsshack]
 }
